c75a911cec6ca326484d20308be0b8586b674acb
Jackson <36348184+toytwo@users.noreply.github.com>
2024-05-08 14:13:41 -0500

    Turned off developer tools

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
  * @version Updated: 5/8/2024
  */
 public class Main {
-    public final static boolean skipMenu = true; // Developer tool for skipping the main menu
+    public final static boolean skipMenu = false; // Developer tool for skipping the main menu
     public final static int defaultGuessPanel = 1; // 0 - SimpleGuess | 1 - ScoreGuess
     public final static int defaultRevealPanel = 2; // 0 - SimpleReveal | 1 - ColorReveal | 2 - SpotlightReveal
     public final static int defaultDifficulty = 1; // 0 - EASY | 1 - MEDIUM | 2 - HARD
